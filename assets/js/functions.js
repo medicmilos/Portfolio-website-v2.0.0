5d3a6cac074e25fe60d868b7d06a3034572d1f29
Milos Medic <milos.medic.130.14@ict.edu.rs>
2017-06-23 20:51:47 +0200

    v2

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,7 +1,7 @@
 $(function() {
 	smoothScroll(300);
-	workBelt();
-	workLoad();
+	//workBelt();
+	//workLoad();
 });
 
 function smoothScroll (duration) {
@@ -16,35 +16,6 @@ function smoothScroll (duration) {
 	        }, duration);
 	    }
 	});
-}
-
-function workBelt() {
-
-  $(".trigger").remove();
-  $(".return").remove();
-
-  $('.thumb-container label').click(function() {
-    $('.work-belt').addClass("slided");
-    $('.work-container').show();
-  });
-
-  $('.work-return').click(function() {
-    $('.work-belt').removeClass("slided");
-    $('.work-container').hide(800);
-  });
-}
-
-function  workLoad() {
-  $.ajaxSetup({ cache: true });
-  $('.thumb-container label').click(function() {
-    var $this = $(this),
-        newTitle = $this.find('strong').text(),
-        spinner = '<div class="loader">Loading...</div>',
-        url = $this.find('.thumb-unit').data('url'); //comment
-
-    $('.project-load').html(spinner).load(url);
-    $('.project-title').text(newTitle);
-  });
 }
 
 function initMap() {
@@ -117,38 +88,194 @@ function initMap() {
     });
 }
 
-function contactformsend(){
-    var email = $("#email").val() ,
-        fullname = $("#fullname").val(),
-        message = $("#message").val();
-    var _fullname = /^[\w\s]+$/,
-        _email = /^\S+\@\S+\.\S{2,4}$/;
+$(document).ready(function() {
+    $('#contactForm')
+        .formValidation({
+            framework: 'bootstrap',
+            icon: {
+                valid: 'glyphicon glyphicon-ok',
+                invalid: 'glyphicon glyphicon-remove',
+                validating: 'glyphicon glyphicon-refresh'
+            },
+						fields: {
+ 						 name: {
+ 								 validators: {
+ 										 notEmpty: {
+ 												 message: 'The email address is required'
+ 										 },
+ 		                    regexp: {
+ 		                        regexp: /^[A-Z][A-z]{2,}\s[A-Z][A-z]{2,}$/,
+ 		                        message: 'The full name must start with capital letter'
+ 		                    }
+ 								 }
+ 						 },
+ 							 _replyto: {
+ 									 validators: {
+ 											 notEmpty: {
+ 													 message: 'The email address is required'
+ 											 },
+ 											 emailAddress: {
+ 													 message: 'The input is not a valid email address'
+ 											 }
+ 									 }
+ 							 },
+ 							 message: {
+ 									 validators: {
+ 											 notEmpty: {
+ 													 message: 'The message is required'
+ 											 }
+ 									 }
+ 							 }
+ 					 }
+        })
 
-    var num = 0;
+				var canvas;
+				var ctx;
+				var MaxCircles = 175;
+				var particles = [];
 
-    if(!_fullname.test(fullname))
-    {
-        num++;
-        errorMessage("#fullname");
-    }
-    if(!_email.test(email.trim()))
-    {
-        num++;
-        errorMessage("#email");
-    }
-    if(message.length <= 10)
-    {
-        num++;
-        errorMessage("#message");
-    }
+				function main(){
+				    canvas = document.getElementById("canvas");
+					ctx = canvas.getContext("2d");
 
-    if(num > 0){
-        return false;
-    }
+				    var frame = function(){
+						draw();
+						update();
+					window.requestAnimationFrame(frame,canvas);
 
-    return true;
-}
+					};
+					window.requestAnimationFrame(frame,canvas);
+				}
 
-function errorMessage(id){
-    $(id).parent().children(".error-message").css("opacity","1");
-}
+				function Circles(x,y,xSpeed,ySpeedS){
+					this.radius = 0.75;//15;
+					circlePos();
+					this.x = circleX;
+					this.y = circleY;
+					this.xSpeed = (Math.random() - Math.random()); //2
+				    this.ySpeed = (Math.random() - Math.random()); //2
+					this.color = '#D80000';
+				}
+
+				main();
+
+				function circlePos(){
+					circleX = (canvas.width-2)*Math.random();
+					circleY = (canvas.height-2)*Math.random();
+				}
+
+				for(i=1; i<MaxCircles; i++){
+					particles[i] = new Circles();
+				}
+
+				particles[0] = {};
+
+				function draw(){
+					ctx.clearRect(0,0,canvas.width,canvas.height);
+					drawCircles();
+				}
+
+
+				function update(){
+					updateCircles();
+				}
+
+				function updateCircles(){
+					for(i=1; i<particles.length; i++){
+						particles[i].x += particles[i].xSpeed;
+						particles[i].y += particles[i].ySpeed;
+
+						if(particles[i].x + particles[i].radius > canvas.width ){
+							particles[i].x = canvas.width - particles[i].radius;
+							particles[i].xSpeed =-particles[i].xSpeed;
+				        }
+				        if(particles[i].x < particles[i].radius){
+							particles[i].x = particles[i].radius;
+							particles[i].xSpeed =-particles[i].xSpeed;
+						}
+				        if(particles[i].y  + particles[i].radius > canvas.height){
+							particles[i].y = canvas.height - particles[i].radius;
+							particles[i].ySpeed =-particles[i].ySpeed;
+				        }
+				        if(particles[i].y < particles[i].radius){
+							particles[i].y = particles[i].radius;
+							particles[i].ySpeed =-particles[i].ySpeed;
+				        }
+					}
+				}
+
+				function drawCircles(){
+					for(i=0; i<particles.length; i++){
+					    ctx.beginPath();
+						ctx.arc(particles[i].x, particles[i].y, particles[i].radius, 0, 2 * Math.PI, false);
+				     	ctx.fill();
+						ctx.stroke();
+						ctx.fillStyle = 'white';
+
+						for(j=0; j<particles.length; j++){
+						    var dis = Math.sqrt(Math.pow((particles[j].x - particles[i].x),2) +  Math.pow((particles[j].y - particles[i].y),2));
+
+							if(dis<100){
+
+							var width;
+
+							if( 1 > 200/(dis*dis) ){
+							   width = 200/(dis*dis);
+							}else{
+							   width = 0.7;
+							}
+							ctx.beginPath();
+							ctx.moveTo(particles[i].x,particles[i].y);
+							ctx.lineTo(particles[j].x, particles[j].y);
+							ctx.strokeStyle = 'white';
+							ctx.lineWidth = width;
+							ctx.stroke();
+							}
+						}
+					}
+				}
+
+				function getMousePos(canvas, evt) {
+					var rect = canvas.getBoundingClientRect();
+					return {
+					  x: evt.clientX - rect.left,
+					  y: evt.clientY - rect.top
+					};
+				}
+
+				canvas.addEventListener('mousemove', function(evt){
+					var mousePos = getMousePos(canvas, evt);
+					particles[0] = {
+						radius:0.5,
+						x: mousePos.x,
+						y: mousePos.y
+					}
+				}, false);
+
+				canvas.addEventListener('click', function(evt){
+					var mousePos = getMousePos(canvas, evt);
+					   particles.push({
+							radius:0.75,
+							x: mousePos.x,
+							y: mousePos.y,
+							xSpeed:(Math.random() - Math.random()),
+							ySpeed:(Math.random() - Math.random())
+						},{radius:0.75,
+							x: mousePos.x,
+							y: mousePos.y,
+							xSpeed:(Math.random() - Math.random()),
+							ySpeed:(Math.random() - Math.random())
+						},{radius:0.75,
+							x: mousePos.x,
+							y: mousePos.y,
+							xSpeed:(Math.random() - Math.random()),
+							ySpeed:(Math.random() - Math.random())
+						},{radius:0.75,
+							x: mousePos.x,
+							y: mousePos.y,
+							xSpeed:(Math.random() - Math.random()),
+							ySpeed:(Math.random() - Math.random())
+						});
+
+				}, false);
+});
